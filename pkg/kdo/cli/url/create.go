336db2090e2a357beeb053cb27d177dd592bd0fd
Stephanie <stephanie.cao@ibm.com>
2019-09-12 15:50:08 -0400

    first commit for url

---
+++ pkg/kdo/cli/url/create.go
@@ -0,0 +1,126 @@
+package url
+
+import (
+	"fmt"
+
+	// "github.com/openshift/odo/pkg/odo/util/completion"
+	"github.com/pkg/errors"
+	"github.com/redhat-developer/odo-fork/pkg/config"
+	"github.com/redhat-developer/odo-fork/pkg/kdo/genericclioptions"
+	"github.com/redhat-developer/odo-fork/pkg/log"
+	"github.com/redhat-developer/odo-fork/pkg/url"
+
+	"github.com/redhat-developer/odo-fork/pkg/util"
+	"github.com/spf13/cobra"
+	ktemplates "k8s.io/kubectl/pkg/util/templates"
+)
+
+const createRecommendedCommandName = "create"
+
+var (
+	urlCreateShortDesc = `Create a URL for a component`
+	urlCreateLongDesc  = ktemplates.LongDesc(`Create a URL for a component.
+
+	The created URL can be used to access the specified component from outside the OpenShift cluster.
+	`)
+	urlCreateExample = ktemplates.Examples(`  # Create a URL for the current component with a specific port
+	%[1]s --port 8080
+  
+	# Create a URL with a specific name and port
+	%[1]s example --port 8080
+  
+	# Create a URL with a specific name by automatic detection of port (only for components which expose only one service port) 
+	%[1]s example
+  
+	# Create a URL with a specific name and port for component frontend
+	%[1]s example --port 8080 --component frontend
+	  `)
+)
+
+// URLCreateOptions encapsulates the options for the odo url create command
+type URLCreateOptions struct {
+	localConfigInfo  *config.LocalConfigInfo
+	componentContext string
+	urlName          string
+	urlPort          int
+	ingressDomain    string
+	componentPort    int
+	*genericclioptions.Context
+}
+
+// NewURLCreateOptions creates a new UrlCreateOptions instance
+func NewURLCreateOptions() *URLCreateOptions {
+	return &URLCreateOptions{}
+}
+
+// Complete completes UrlCreateOptions after they've been Created
+func (o *URLCreateOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
+	o.Context = genericclioptions.NewContext(cmd)
+	o.componentPort, err = url.GetValidPortNumber(o.Client, o.urlPort, o.Component(), o.Application)
+	if err != nil {
+		return err
+	}
+	if len(args) == 0 {
+		o.urlName = url.GetURLName(o.Component(), o.componentPort)
+	} else {
+		o.urlName = args[0]
+		o.ingressDomain = args[1]
+	}
+	o.localConfigInfo, err = config.NewLocalConfigInfo(o.componentContext)
+
+	return
+}
+
+// Validate validates the UrlCreateOptions based on completed values
+func (o *URLCreateOptions) Validate() (err error) {
+
+	// Check if exist
+	for _, localUrl := range o.localConfigInfo.GetUrl() {
+		if o.urlName == localUrl.Name {
+			return fmt.Errorf("the url %s already exists in the application: %s", o.urlName, o.Application)
+		}
+	}
+
+	// Check if url name is more than 63 characters long
+	if len(o.urlName) > 63 {
+		return fmt.Errorf("url name must be shorter than 63 characters")
+	}
+
+	if !util.CheckOutputFlag(o.OutputFlag) {
+		return fmt.Errorf("given output format %s is not supported", o.OutputFlag)
+	}
+
+	return
+}
+
+// Run contains the logic for the odo url create command
+func (o *URLCreateOptions) Run() (err error) {
+	err = o.localConfigInfo.SetConfiguration("url", config.ConfigUrl{Name: o.urlName, Port: o.urlPort, Host: o.ingressDomain})
+	if err != nil {
+		return errors.Wrapf(err, "failed to persist the component settings to config file")
+	}
+	log.Successf("URL %s created", o.urlName)
+	log.Infof("\nRun `kdo push` to apply URL: %s", o.urlName)
+	return
+}
+
+// NewCmdURLCreate implements the odo url create command.
+func NewCmdURLCreate(name, fullName string) *cobra.Command {
+	o := NewURLCreateOptions()
+	urlCreateCmd := &cobra.Command{
+		Use:     name + " [component name]",
+		Short:   urlCreateShortDesc,
+		Long:    urlCreateLongDesc,
+		Example: fmt.Sprintf(urlCreateExample, fullName),
+		Args:    cobra.MaximumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			genericclioptions.GenericRun(o, cmd, args)
+		},
+	}
+	urlCreateCmd.Flags().IntVarP(&o.urlPort, "port", "", -1, "port number for the url of the component, required in case of components which expose more than one service port")
+	_ = urlCreateCmd.MarkFlagRequired("port")
+	genericclioptions.AddOutputFlag(urlCreateCmd)
+	genericclioptions.AddContextFlag(urlCreateCmd, &o.componentContext)
+	// completion.RegisterCommandFlagHandler(urlCreateCmd, "context", completion.FileCompletionHandler)
+	return urlCreateCmd
+}
---
+++ pkg/kdo/cli/url/list.go
@@ -0,0 +1,128 @@
+package url
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	// "github.com/openshift/odo/pkg/odo/util/completion"
+	"github.com/pkg/errors"
+	"github.com/redhat-developer/odo-fork/pkg/config"
+	"github.com/redhat-developer/odo-fork/pkg/kdo/genericclioptions"
+	"github.com/redhat-developer/odo-fork/pkg/log"
+	"github.com/redhat-developer/odo-fork/pkg/url"
+	"github.com/redhat-developer/odo-fork/pkg/util"
+	"github.com/spf13/cobra"
+	ktemplates "k8s.io/kubectl/pkg/util/templates"
+)
+
+const listRecommendedCommandName = "list"
+
+var (
+	urlListShortDesc = `List URLs`
+	urlListLongDesc  = ktemplates.LongDesc(`Lists all the available URLs which can be used to access the components.`)
+	urlListExample   = ktemplates.Examples(` # List the available URLs
+  %[1]s
+	`)
+)
+
+// URLListOptions encapsulates the options for the kdo url list command
+type URLListOptions struct {
+	localConfigInfo  *config.LocalConfigInfo
+	componentContext string
+	*genericclioptions.Context
+}
+
+// NewURLListOptions creates a new UrlCreateOptions instance
+func NewURLListOptions() *URLListOptions {
+	return &URLListOptions{}
+}
+
+// Complete completes UrlListOptions after they've been Listed
+func (o *URLListOptions) Complete(name string, cmd *cobra.Command, args []string) (err error) {
+	o.Context = genericclioptions.NewContext(cmd)
+	o.localConfigInfo, err = config.NewLocalConfigInfo(o.componentContext)
+	if err != nil {
+		return errors.Wrap(err, "failed intiating local config")
+	}
+	return
+}
+
+// Validate validates the UrlListOptions based on completed values
+func (o *URLListOptions) Validate() (err error) {
+	if util.CheckOutputFlag(o.OutputFlag) {
+		return fmt.Errorf("the specified output format is not supported")
+	}
+	return
+}
+
+// Run contains the logic for the kdo url list command
+func (o *URLListOptions) Run() (err error) {
+
+	urls, err := url.List(o.Client, o.Component(), o.Application)
+	if err != nil {
+		return err
+	}
+
+	localUrls := o.localConfigInfo.GetUrl()
+
+	if o.OutputFlag == "json" {
+		out, err := json.Marshal(urls)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(out))
+	} else {
+
+		if len(urls.Items) == 0 && len(localUrls) == 0 {
+			return fmt.Errorf("no URLs found for component %v in application %v", o.Component(), o.Application)
+		} else {
+
+			log.Infof("Found the following URLs for component %v in application %v:", o.Component(), o.Application)
+
+			tabWriterURL := tabwriter.NewWriter(os.Stdout, 5, 2, 3, ' ', tabwriter.TabIndent)
+
+			//create headers
+			fmt.Fprintln(tabWriterURL, "NAME", "\t", "URL", "\t", "PORT")
+
+			for _, i := range localUrls {
+				var present bool
+				for _, u := range urls.Items {
+					if i.Name == u.Name {
+						fmt.Fprintln(tabWriterURL, u.Name, "\t", url.GetURLString(u.Spec.Protocol, u.Spec.Host), "\t", u.Spec.Port)
+						present = true
+					}
+				}
+				if !present {
+					fmt.Fprintln(tabWriterURL, i.Name, "\t", "<not created on cluster>", "\t", i.Port)
+				}
+			}
+
+			tabWriterURL.Flush()
+			fmt.Println("\nUse `kdo push` to create url on cluster")
+		}
+
+	}
+	return
+}
+
+// NewCmdURLList implements the kdo url list command.
+func NewCmdURLList(name, fullName string) *cobra.Command {
+	o := NewURLListOptions()
+	urlListCmd := &cobra.Command{
+		Use:     name,
+		Short:   urlListShortDesc,
+		Long:    urlListLongDesc,
+		Example: fmt.Sprintf(urlListExample, fullName),
+		Args:    cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			genericclioptions.GenericRun(o, cmd, args)
+		},
+	}
+	genericclioptions.AddOutputFlag(urlListCmd)
+	genericclioptions.AddContextFlag(urlListCmd, &o.componentContext)
+	// completion.RegisterCommandFlagHandler(urlListCmd, "context", completion.FileCompletionHandler)
+
+	return urlListCmd
+}
